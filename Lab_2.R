0e2c33b40b454a187d92385837e0a51434eb73d7
Yurii Zhuk <yurzhuk@icloud.com>
2019-01-31 13:35:28 +0200

    naming errors corrected

--- Lab_2.R
+++ Lab_2.R
@@ -4,15 +4,14 @@ setwd('~/LAB2')
 
 movie_body_counts <- read.csv ( 'filmdeathcounts.csv')
 
-movie_body_counts$body_per_min <- movie_body_counts$Body_Cou nt/movie_body_counts$Length_Minutes
+movie_body_counts$body_per_min <- movie_body_counts$Body_Count / movie_body_counts$Length_Minutes
 
 ggplot(movie_body_counts, aes(x=Body_Count)) + geom_histogram(bins=20, color="grey", fill="lightblue")
 
 movie_body_counts %>% top_n(n = 10, Body_Count) %>% arrange(desc(Body_Count))
 
-ggplot(movie_body_counts, aes(x=IMDB_Rating)) + geom_histogram(bins=10, color="grey", fill="lightblue")
-
 movie_body_counts %>% top_n(n = 10, body_per_min) %>% arrange(desc(body_per_min))
+
 
 imdb_mean <- mean(movie_body_counts[["IMDB_Rating"]])
 
